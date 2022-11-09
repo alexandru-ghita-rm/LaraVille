b91ea67802e9829dd1fcc1f5087ea2a6be36c33e
Alexandru Ghita <alexandru.ghita@imobiliare.ro>
2022-11-09 20:28:16 +0200

    Getting started

---
+++ resources/views/post.blade.php
@@ -0,0 +1,9 @@
+<x-layout>
+    <article>
+        <h1>{{$post->title}}</h1>
+        <div>
+            {!! $post->body !!}
+        </div>
+    </article>
+    <a href="/"> go backs </a></a>
+</x-layout>
